5ee4e631cd9d4dda595c541014be55bba400ac81
Stephanie Walsh <srwalsh21@gmail.com>
2022-03-22 11:46:11 -0400

    Added table to homepage

--- Downloads/550-final-master/client/src/components/Chains.js
+++ Downloads/550-final-master/client/src/components/Chains.js
@@ -4,13 +4,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import PageNavbar from './PageNavbar';
 import { Jumbotron } from 'react-bootstrap';
 import './Chains.css';
+//import { Button } from '@material-ui/core';
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 
 export default class Chains extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      clicks: 0
+      clicks: 0,
+      rows: []
     };
 
     // You must always call the update function 
@@ -23,17 +34,19 @@ export default class Chains extends React.Component {
   componentDidMount() {
     //set initial state here
     this.setState({
-      clicks: 0
+      clicks: 0,
+      rows: []
     });
   };
     
   // sample state change function
   handleClick(event) {
     this.setState({
-        clicks: this.state.clicks + 1
+        clicks: this.state.clicks + 1,
+        rows: [...this.state.rows, {'id': 1, 'type': 2, 'notes': 3, 'verify': 'YES'}]
     });
+    console.log(this.state);
   }
-
 
   render() {
     return (
@@ -51,6 +64,40 @@ export default class Chains extends React.Component {
            <p>Num clicks: {this.state.clicks} </p>
               
           <br></br>
+
+          
+
+       
+          
+          <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Type</TableCell>
+            <TableCell>Notes</TableCell>
+            <TableCell align="right">Verified</TableCell>
+          </TableRow>
+        </TableHead>
+        
+        <TableBody>
+         
+          
+          {this.state.rows.map((row) => (
+            <TableRow>
+              <TableCell> {row.id}</TableCell>
+              <TableCell>{row.type}</TableCell>
+              <TableCell>{row.notes}</TableCell>
+              <TableCell align="right">{row.verify}</TableCell>
+            </TableRow>
+          ))}
+
+        
+          
+        </TableBody>
+
+      </Table>
+    </TableContainer> 
 
         </Jumbotron>
       </div>
